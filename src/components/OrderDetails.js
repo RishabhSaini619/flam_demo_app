edfdfb9dee35e7c62cab67e36f586ff7f7e2d2ee
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-06-16 13:16:00 +0530

    v1.1-Empty photo & video URL

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -8,12 +8,11 @@ const orderDetails = {
   varientId: "fe6748e1-6b3d-4db1-817e-57575f78f42f",
   refId: refrenceId,
   photo: {
-    url: "https://s3-ap-south-1.amazonaws.com/flam-videoshop-assets/flam/prod/base/efb45ee6-a22a-41b3-931b-49c171075a5a.png",
+    url: "",
   },
   video: {
-    url: "https://flam-videoshop-assets.s3.ap-south-1.amazonaws.com/flam/app/videos/1_LT_Flam.mp4",
-    default:
-      "https://flam-videoshop-assets.s3.ap-south-1.amazonaws.com/flam/app/videos/1_LT_Flam.mp4",
+    url: "",
+    default: "",
   },
   color: "#000000",
 };
