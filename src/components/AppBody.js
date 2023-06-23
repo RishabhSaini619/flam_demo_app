23f91640e909234fde4186af119341e62ba5d825
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-06-23 15:53:13 +0530

    new destop ui

--- src/components/AppBody.js
+++ src/components/AppBody.js
@@ -35,7 +35,7 @@ const AppBody = ({ orderData, orderStatusData, onLaunch, onRefresh }) => {
           {/* card photo */}
           <div className="card-photo">
             {/* card - title  */}
-            <h4 className="card-title">{"Photo Preview"}</h4>
+            <h4 className="card-title">Photo Preview</h4>
             {/* card -preview */}
             <img
               className="client_photo_url"
@@ -43,12 +43,49 @@ const AppBody = ({ orderData, orderStatusData, onLaunch, onRefresh }) => {
               alt="image"
             />
             {/* card btn title */}
-            
-            <h4 className="card-btn" onClick={downloadImage} style={{ color: '#E0FE69'}}>{"Download Photo"}</h4>
-            
+
+            <h4 className="card-btn" onClick={downloadImage}>
+              {"Download Photo"}
+            </h4>
           </div>
-          <div className="card-video">
-          <h4 className="card-title">{"Video Preview"}</h4>
+          <div className="card-video-1">
+            <h4 className="card-title">{"Video Preview"}</h4>
+            <div className="client_video_url">
+              <ReactPlayer
+                url={orderData.video_url}
+                width="120%"
+                height="100%"
+                controls={true}
+              />
+            </div>
+            <h4
+              className="card-btn"
+              onClick={downloadVideo}
+              style={{ color: "#E0FE69" }}
+            >
+              {"Download Video "}
+            </h4>
+          </div>
+          <div className="card-stats">
+            <h4 className="card-title">Status</h4>
+            <h4
+              className="card-stats-statusbar"
+              style={{
+                color:
+                  orderStatusData.status == "PROCESSING" ? "orange" : "green",
+              }}
+            >
+              {orderStatusData && orderStatusData.status}
+            </h4>
+            <img
+              src={process.env.PUBLIC_URL + "refresh.png"}
+              alt="image"
+              className="refresh"
+              onClick={onRefresh}
+            />
+          </div>
+          <div className="card-video-2">
+            <h4 className="card-title">{"Video Preview"}</h4>
             <div className="client_video_url">
               <ReactPlayer
                 url={orderData.video_url}
@@ -56,23 +93,18 @@ const AppBody = ({ orderData, orderStatusData, onLaunch, onRefresh }) => {
                 height="100%"
                 controls={true}
               />
-            </div> 
-            <h4 className="card-btn" onClick={downloadVideo} style={{ color: '#E0FE69'}}>{"Download Video "}</h4>
-          </div>
-          <div className="card-stats">
-            <h4>Status</h4>
-            <h4 className="card-stats-statusbar" style={{ color: orderStatusData.status=="PROCESSING" ? ("orange"):('green')}}>{orderStatusData && orderStatusData.status}</h4>
-            <img 
-              src={process.env.PUBLIC_URL + "refresh.png"}
-              alt="image"
-              className="refresh"
-              width="20"
-              height="20"
-              onClick={onRefresh}
-            />
+            </div>
+            <h4
+              className="card-btn"
+              onClick={downloadVideo}
+              style={{ color: "#E0FE69" }}
+            >
+              {"Download Video "}
+            </h4>
           </div>
           <div className="logo_zingcam">
-            <img className="logo_zingcam_photo"
+            <img
+              className="logo_zingcam_photo"
               src={process.env.PUBLIC_URL + " logo_zingcam.png"}
               alt="image"
               onClick={() =>
@@ -83,7 +115,9 @@ const AppBody = ({ orderData, orderStatusData, onLaunch, onRefresh }) => {
         </div>
       ) : (
         <div className="App-body-submit">
-          <h4 className="App-body-submit-launch" onClick={onLaunch}>{"LAUNCH"}</h4>
+          <h4 className="App-body-submit-launch" onClick={onLaunch}>
+            {"LAUNCH"}
+          </h4>
         </div>
       )}
     </div>
